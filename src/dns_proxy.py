fae7f47d094fc7ce791f933f6dde910c36fd7421
Mohammad Mahdi Gholami <118852638+mohami19@users.noreply.github.com>
2023-06-29 18:17:36 +0000

    renamed some variable && Corrected the Indentation

--- src/dns_proxy.py
+++ src/dns_proxy.py
@@ -2,8 +2,8 @@ import socket
 import time
 import json
 import redis
-import Datetime
-
+import datetime
+import struct
 
 def parse_dns_packet(packet):
     header = packet[:12]
@@ -19,12 +19,32 @@ def parse_dns_packet(packet):
     extra = packet[i+4:]
     return header, qname[:-1], qtype, qclass ,extra
 
+def db_select():
+    r = redis.Redis(host='localhost', port=6379, db=0)
+    cache = {}
+    keys = r.keys('*')
+    for key in keys:
+        item = r.lpop(key)
+        while(item != None):
+            if key in cache:
+                cache[key].append(item)
+            else:
+                cache[key] = [item]
+            item = r.lpop(key)
+    return cache
+
+def db_insert(key, result):
+    r = redis.Redis(host='localhost', port=6379, db=0)
+    r.lpush(key, int((datetime.datetime.today()-datetime.datetime(2023,1,1,0,0,0,0)).total_seconds()))
+    for res in result:
+        r.lpush(key, res)
+
 with open('setting.json') as file:
     setting = json.load(file)
 expiration_time = setting['cache_expiration_time']
 HOST = "127.0.0.3"
 PORT = 5006
-
+cache_redis = db_select()
 s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 s.bind((HOST, PORT))
 
@@ -38,20 +58,22 @@ while True:
     data,addr = s.recvfrom(512)
     
     if data in cache:
-        
         print("in dns cache")
     else:
         sock.sendto(data, ('8.8.8.8', 53))
-        respose_date, respose_addr = sock.recvfrom(1024)
+        response_data, response_addr = sock.recvfrom(1024)
         result = (socket.getaddrinfo(parse_dns_packet(response_data)[1],80))
-        res = {result[0][4][0]}
+        IPs = {result[0][4][0]}
         for i in result:
-            res.add(i[4][0])
+            IPs.add(i[4][0])
+        cache.update({data : IPs})
+        db_insert(parse_dns_packet(response_data)[1], IPs)
         
     clear_cache = time.time()
     if clear_cache - start >= float(expiration_time):
         cache.clear()
+        cache_redis.clear()
         start = time.time()
     
-    for i in res:
+    for i in IPs:
         s.sendto(i.encode(),addr)
